2eef3c8796c6b5a6ee44fb7832f51981c898b393
Jose Lopez <codeandbachata@gmail.com>
2025-06-13 07:49:16 -0400

    fix: Change router history mode to use createWebHashHistory for better compatibility

--- src/router.js
+++ src/router.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import Home from './components/Home.vue' // Import the new Home component
 import Gallery from './components/Gallery.vue'
 
@@ -8,7 +8,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes
 })
 
